845496641c007631d813e8fd52e8ea14a5d5b17a
rafiasana324-del <rafiasana324@gmail.com>
2026-07-02 05:08:23 +0000

    container config change

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 11223
+let accountemail ="rafiasana234@gmail.com"
+var accountpassword ="123456"
+accountcity ="phalia"
+ let accountstate;
+
+//noaccountId = 123
+console.log(accountId);
+accountemail ="hchc@123gmail.com"
+console.log(accountemail);
+accountpassword ="123"
+console.table([accountcity,accountemail,accountId,accountstate,accountpassword])
+
+/*
+prefer not use var 
+because it create may manipulation its create issue in block scope 
+and functional scope 
+*/ 
