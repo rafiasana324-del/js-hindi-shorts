7c81f5142493ba67d91b16faba66730f013922f3
rafiasana324-del <rafiasana324@gmail.com>
2026-07-02 06:23:01 +0000

    container config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"// treat all js code in newer version 
+ //alert( 3 + 3 )// we are using nodejs not browser 
+ // numner => 2 to power 53
+ //bigint for big values
+ //boolian =>  ture,false
+ //string => ""
+ //null => standalone value 
+ //undefined => 
+// symbol => unique 
+ //object 
+ let name ="rafia"
+ let age = 19
+ let inlogedin = true
+ let state = null
+ let id ="12"
+ let abc;
+ console.log([name,age,inlogedin,state,id])
+ console.log(typeof "rafia")
+ console.log(typeof age)
+ console.log(typeof null); // object
+ console.log(typeof undefined); //type in js
+ 
+ 
+ 
