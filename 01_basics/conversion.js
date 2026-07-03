4ba42b5443a89efeaee7e17cb1db81945fdd0e7a
rafiasana324-del <rafiasana324@gmail.com>
2026-07-03 06:18:03 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+console.log(typeof score)//string
+let valueinNumber =Number(score)
+console.log(typeof valueinNumber)//number
+console.log(valueinNumber)//nan
+
+
+let age = "12abc"
+
+console.log(typeof age)//string
+let ageinNumber =Number(age)
+console.log(typeof ageinNumber)//number
+console.log(ageinNumber)//nan
+
+let value = null
+console.log(value)
+console.log(typeof value)
+
+let value2 = undefined
+console.log(value2)
+console.log(typeof value2)
+ 
+let value3 = true// true or false
+console.log(value3)
+console.log(typeof value3)
+
+let isloggedIn = 1 // 1=> true 0=> false empty=> false anyvalue=> ture
+console.log(isloggedIn)
+let BooleanisloggedIn =Boolean(isloggedIn)
+console.log(typeof BooleanisloggedIn)
+console.log(BooleanisloggedIn)
+
