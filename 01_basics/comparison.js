a6df26367a9dadea9df6094c288888613108e90e
rafiasana324-del <rafiasana324@gmail.com>
2026-07-04 15:58:30 +0000

    container config done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,32 @@
+// simple comparison string and values 
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+//not good way use same datatype  
+console.log("2" > 1);
+console.log(2 == "1");
+
+//not good way system create own value in the place of null
+console.log( null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+
+// all result of undefined value is zero
+console.log( undefined == 0);
+console.log(undefined  < 1);
+console.log(undefined >= 2);
+  
+//=== this using follow change method compared to other its sticly check the datatype and vakue 
+console.log(2 === 1);
+console.log("2" === 1);
+console.log("1" === 1);
+
+ 
+
+
+
+
+
