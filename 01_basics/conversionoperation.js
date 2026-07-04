d78f9db77f052d099aaf9946a532f78a23cf7e55
rafiasana324-del <rafiasana324@gmail.com>
2026-07-04 06:32:15 +0000

    container config done

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,49 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+ 
+//using operation
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 ** 2);
+console.log(2 % 2);
+ 
+//we add also string value
+let str1 = "hello"
+let str2 = "  rafia"
+let str3 = str1 + str2
+console.log(str3);
+
+//these are rules of js
+let value1 = 2
+let value2 = 4
+console.log( 2 + "4");
+console.log( "2" + 4);
+console.log("1" + 2 + 4)
+console.log(1 + 2 + "4");
+ 
+// boolean values 
+console.log(+true);
+console.log(+"");
+console.log(-false);
+ 
+// good way for soulution by using brakits 
+console.log((2+2)*4-6);
+
+
+// not good way 
+let num1, num2,num3 
+num1=num2=num3= 2+2
+
+//prefix , postfix
+let gamecounter = 33
+gamecounter++;
+console.log(gamecounter);
+
+let gamecounter1 = 33
+--gamecounter1;
+console.log(gamecounter1);
+
+
