58e73df2b29179cba4d612a67830784ff44fc2e7
rafiasana324-del <rafiasana324@gmail.com>
2026-07-27 07:03:05 +0000

    container config done

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,3 +1,27 @@
+const name = "rafia"
+const value = 10;
+console.log(name + value)
+console.log('hellow my name is ${rafia} and my value is ${value}');
 
+const gameName = new String("rafiasana")
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('i'));
+console.log(gameName.charAt(3));
 
-c
+const gameName2 = new String("zia_ullah")
+const newString = gameName2.substring(0,4);
+console.log(newString);
+const anotherString = gameName2.slice(-8,4)
+console.log(anotherString);
+
+const anotherString2 =     sobii    ;
+console.log(anotherString2);
+console.log(anotherString2.trim());
+
+const url = "https//rafiasana%20choudry.com";
+console.log(url.replace( '%20' , _))
+url.include('rafia')
+console.log(url.include);
